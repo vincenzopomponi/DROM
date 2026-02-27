3e951570ea9fbe162f4d249d39a46773d1fd84f5
Vincenzo Pomponi <vincenzo.pomponi@supsi.ch>
2026-02-27 14:54:56 +0100

    Updated

--- drom/bash/train.sh
+++ drom/bash/train.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-python drom/scripts/train_drom.py --config drom/configs/low_dim/StackReal.json
-
-sleep 10
-
-python drom/scripts/train_drom.py --config drom/configs/low_dim/StackNewReal.json
-
-sleep 10
-
-python drom/scripts/train_drom.py --config drom/configs/low_dim/StackCleanupNewReal.json
-
-sleep 10
-
-python drom/scripts/train_drom.py --config drom/configs/low_dim/StackCleanupNewReal1.json
-
-sleep 10
-
-python drom/scripts/train_drom.py --config drom/configs/low_dim/StackCleanupNewReal2.json
-
-sleep 10
-
-python drom/scripts/train_drom.py --config drom/configs/low_dim/StackCleanupNewReal3.json
+python drom/scripts/train_drom.py --config drom/configs/low_dim/drom.json
---
+++ drom/demos/__init__.py
@@ -0,0 +1,2 @@
+import os
+demo_root = os.path.join(os.path.dirname(__file__), "hdf5")
